d903668470acb7e7a5ce51700af3f60f0ba19862
Nutchaya Vorayingyong <nemopear@gmail.com>
2015-06-03 14:16:24 +0700

    basic slide

--- app.js
+++ app.js
@@ -87,40 +87,218 @@ angular
 	.run(function($rootScope, $state) {
 		$rootScope.$state = $state;
 	})
-	// .controller('TabsCtrl',TabsCtrl);
 
-
-	.controller('SliderController', function($scope) {
+	.controller('WeavingSliderController', function($scope) {
 	  $scope.images = [{
-	    src: 'basket-01.png',
+	    src: 'basket-weaving/basket-01.png',
 	    title: 'Pic 1'
 	  }, {
-	    src: 'basket-02.png',
+	    src: 'basket-weaving/basket-02.png',
 	    title: 'Pic 2'
 	  }, {
-	    src: 'basket-03.png',
+	    src: 'basket-weaving/basket-03.png',
 	    title: 'Pic 3'
 	  }, {
-	    src: 'basket-04.png',
+	    src: 'basket-weaving/basket-04.png',
 	    title: 'Pic 4'
 	  }, {
-	    src: 'basket-05.png',
+	    src: 'basket-weaving/basket-05.png',
 	    title: 'Pic 5'
 	  }, {
-			src: 'basket-06.png',
+			src: 'basket-weaving/basket-06.png',
 			title: 'Pic 6'
 	  }, {
-	    src: 'basket-07.png',
+	    src: 'basket-weaving/basket-07.png',
 	    title: 'Pic 7'
 	  }, {
-	    src: 'basket-08.png',
+	    src: 'basket-weaving/basket-08.png',
 	    title: 'Pic 8'
 	  }, {
-	    src: 'basket-09.png',
+	    src: 'basket-weaving/basket-09.png',
 	    title: 'Pic 9'
 	  }, {
-	    src: 'basket-10.png',
+	    src: 'basket-weaving/basket-10.png',
 	    title: 'Pic 10'
+	  }];
+	})
+
+	.controller('BlungingSliderController', function($scope) {
+	  $scope.images = [{
+	    src: 'blunging/01.png',
+	    title: 'Pic 1'
+	  }, {
+	    src: 'blunging/02.png',
+	    title: 'Pic 2'
+	  }, {
+	    src: 'blunging/03.png',
+	    title: 'Pic 3'
+	  }, {
+	    src: 'blunging/04.png',
+	    title: 'Pic 4'
+	  }, {
+	    src: 'blunging/05.png',
+	    title: 'Pic 5'
+	  }, {
+			src: 'blunging/06.png',
+			title: 'Pic 6'
+	  }, {
+	    src: 'blunging/07.png',
+	    title: 'Pic 7'
+	  }, {
+	    src: 'blunging/08.png',
+	    title: 'Pic 8'
+	  }, {
+	    src: 'blunging/09.png',
+	    title: 'Pic 9'
+	  }, {
+	    src: 'blunging/010.png',
+	    title: 'Pic 10'
+	  }, {
+	    src: 'blunging/011.png',
+	    title: 'Pic 11'
+	  }];
+	})
+
+	.controller('StitchingSliderController', function($scope) {
+	  $scope.images = [{
+	    src: 'stitching/01.png',
+	    title: 'Pic 1'
+	  }, {
+	    src: 'stitching/02.png',
+	    title: 'Pic 2'
+	  }, {
+	    src: 'stitching/03.png',
+	    title: 'Pic 3'
+	  }, {
+	    src: 'stitching/04.png',
+	    title: 'Pic 4'
+	  }, {
+	    src: 'stitching/05.png',
+	    title: 'Pic 5'
+	  }, {
+			src: 'stitching/06.png',
+			title: 'Pic 6'
+	  }, {
+	    src: 'stitching/07.png',
+	    title: 'Pic 7'
+	  }, {
+	    src: 'stitching/08.png',
+	    title: 'Pic 8'
+	  }, {
+	    src: 'stitching/09.png',
+	    title: 'Pic 9'
+	  }, {
+	    src: 'stitching/010.png',
+	    title: 'Pic 10'
+	  }];
+	})
+
+	.controller('CastingSliderController', function($scope) {
+	  $scope.images = [{
+	    src: 'casting/01.png',
+	    title: 'Pic 1'
+	  }, {
+	    src: 'casting/02.png',
+	    title: 'Pic 2'
+	  }, {
+	    src: 'casting/03.png',
+	    title: 'Pic 3'
+	  }];
+	})
+
+	.controller('WoodworkSliderController', function($scope) {
+	  $scope.images = [{
+	    src: 'woodwork/01.png',
+	    title: 'Pic 1'
+	  }, {
+	    src: 'woodwork/02.png',
+	    title: 'Pic 2'
+	  }, {
+	    src: 'woodwork/03.png',
+	    title: 'Pic 3'
+	  }];
+	})
+
+	.controller('LacqueringSliderController', function($scope) {
+	  $scope.images = [{
+	    src: 'lacquering/01.png',
+	    title: 'Pic 1'
+	  }, {
+	    src: 'lacquering/02.png',
+	    title: 'Pic 2'
+	  }, {
+	    src: 'lacquering/03.png',
+	    title: 'Pic 3'
+	  },  {
+	    src: 'lacquering/04.png',
+	    title: 'Pic 4'
+	  },  {
+	    src: 'lacquering/05.png',
+	    title: 'Pic 5'
+	  },  {
+	    src: 'lacquering/06.png',
+	    title: 'Pic 6'
+	  },  {
+	    src: 'lacquering/07.png',
+	    title: 'Pic 7'
+	  },  {
+	    src: 'lacquering/08.png',
+	    title: 'Pic 8'
+	  },  {
+	    src: 'lacquering/09.png',
+	    title: 'Pic 9'
+	  },  {
+	    src: 'lacquering/010.png',
+	    title: 'Pic 10'
+	  },  {
+	    src: 'lacquering/011.png',
+	    title: 'Pic 11'
+	  },  {
+	    src: 'lacquering/012.png',
+	    title: 'Pic 12'
+	  }];
+	})
+
+	.controller('PaintingSliderController', function($scope) {
+	  $scope.images = [{
+	    src: 'painting/01.png',
+	    title: 'Pic 1'
+	  }, {
+	    src: 'painting/02.png',
+	    title: 'Pic 2'
+	  }, {
+	    src: 'painting/03.png',
+	    title: 'Pic 3'
+	  },  {
+	    src: 'painting/04.png',
+	    title: 'Pic 4'
+	  },  {
+	    src: 'painting/05.png',
+	    title: 'Pic 5'
+	  },  {
+	    src: 'painting/06.png',
+	    title: 'Pic 6'
+	  },  {
+	    src: 'painting/07.png',
+	    title: 'Pic 7'
+	  },  {
+	    src: 'painting/08.png',
+	    title: 'Pic 8'
+	  },  {
+	    src: 'painting/09.png',
+	    title: 'Pic 9'
+	  },  {
+	    src: 'painting/010.png',
+	    title: 'Pic 10'
+	  },  {
+	    src: 'painting/011.png',
+	    title: 'Pic 11'
+	  },  {
+	    src: 'painting/012.png',
+	    title: 'Pic 12'
+	  },  {
+	    src: 'painting/013.png',
+	    title: 'Pic 13'
 	  }];
 	})
 
